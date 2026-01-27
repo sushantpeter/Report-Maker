213fa5ce5fd7990312928ddca4fce959a5c3be06
sushantpeter <58446962+sushantpeter@users.noreply.github.com>
2026-01-27 22:57:49 +0530

    Update readExcel.js

--- utils/readExcel.js
+++ utils/readExcel.js
@@ -1,1 +1,21 @@
+const XLSX = require("xlsx");
+
+/**
+ * Reads all sheets from an Excel file
+ */
+function readExcel(filePath) {
+  const workbook = XLSX.readFile(filePath);
+  let data = {};
+
+  workbook.SheetNames.forEach(sheet => {
+    data[sheet] = XLSX.utils.sheet_to_json(
+      workbook.Sheets[sheet],
+      { defval: "" }
+    );
+  });
+
+  return data;
+}
+
+module.exports = { readExcel };
 
